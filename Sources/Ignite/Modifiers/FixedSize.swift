543a7d8d23620637c68e631295f35b9116febfb2
JP Toro <jp@jptoro.dev>
2025-05-06 15:08:00 -0400

    Improve implementation of `fixedSize()`.

--- Sources/Ignite/Modifiers/FixedSize.swift
+++ Sources/Ignite/Modifiers/FixedSize.swift
@@ -9,7 +9,14 @@ public extension HTML {
     /// Forces an element to be sized based on its content rather than expanding to fill its container.
     /// - Returns: A modified copy of the element with fixed sizing applied
     func fixedSize() -> some HTML {
-        Section(self)
-            .style(.display, "inline-block")
+        self.style(.width, "fit-content")
+    }
+}
+
+public extension InlineElement {
+    /// Forces an element to be sized based on its content rather than expanding to fill its container.
+    /// - Returns: A modified copy of the element with fixed sizing applied
+    func fixedSize() -> some InlineElement {
+        self.style(.width, "fit-content")
     }
 }
