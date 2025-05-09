a620c9f77b1c767a37ecd4a240c2c660cf9a3d3a
JP Toro <jp@jptoro.dev>
2025-05-09 18:00:40 -0400

    Update `Material` to use new `preferredColorScheme()` modifier. Clean up `Material` CSS classes.

--- Sources/Ignite/Framework/Material.swift
+++ Sources/Ignite/Framework/Material.swift
@@ -8,54 +8,33 @@
 /// A type that represents different material effects that can be applied to views.
 /// Material effects combine translucency, blur, and color to create the appearance
 /// of frosted glass. The effect automatically adapts to light and dark color schemes.
-public struct Material: Sendable {
-    /// The type of material effect
-    private let type: MaterialType
-    /// Optional color scheme override
-    private let colorScheme: ColorScheme?
-
-    private enum MaterialType: String {
-        case ultraThin = "ultra-thin"
-        case thin = "thin"
-        case regular = "regular"
-        case thick = "thick"
-        case ultraThick = "ultra-thick"
-    }
-
-    /// Creates a new material with the specified type
-    private init(type: MaterialType, colorScheme: ColorScheme? = nil) {
-        self.type = type
-        self.colorScheme = colorScheme
-    }
-
-    /// Method to override the color scheme
-    /// - Parameter scheme: The color scheme to use
-    /// - Returns: A new Material instance with the specified color scheme
-    public func colorScheme(_ scheme: ColorScheme) -> Material {
-        Material(type: self.type, colorScheme: scheme)
-    }
+public enum Material: Sendable {
+    /// An ultra-thin material effect
+    case ultraThinMaterial
+    /// A thin material effect
+    case thinMaterial
+    /// A regular material effect
+    case regularMaterial
+    /// A thick material effect
+    case thickMaterial
+    /// An ultra-thick material effect
+    case ultraThickMaterial
 
     /// Gets the CSS class name for this material
     var className: String {
-        let baseClass = "material-\(type.rawValue)"
-        if let colorScheme {
-            return baseClass + "-" + colorScheme.rawValue
-        }
-        return baseClass
+        "material-\(rawValue)"
     }
+}
 
-    /// An ultra-thin material effect
-    @MainActor public static let ultraThinMaterial = Material(type: .ultraThin)
-
-    /// A thin material effect
-    @MainActor public static let thinMaterial = Material(type: .thin)
-
-    /// A regular material effect
-    @MainActor public static let regularMaterial = Material(type: .regular)
-
-    /// A thick material effect
-    @MainActor public static let thickMaterial = Material(type: .thick)
-
-    /// An ultra-thick material effect
-    @MainActor public static let ultraThickMaterial = Material(type: .ultraThick)
+extension Material {
+    /// The raw string value for each material type
+    var rawValue: String {
+        switch self {
+        case .ultraThinMaterial: return "ultra-thin"
+        case .thinMaterial: return "thin"
+        case .regularMaterial: return "regular"
+        case .thickMaterial: return "thick"
+        case .ultraThickMaterial: return "ultra-thick"
+        }
+    }
 }
