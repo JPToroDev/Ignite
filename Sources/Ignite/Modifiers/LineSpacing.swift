719110c5d99bdb5790949b3ff5b0414255bfad43
JP Toro <jp@jptoro.dev>
2025-05-23 11:35:10 -0400

    Update `lineSpacing()` to use `LineSpacedHTML` and `Amount`.

--- Sources/Ignite/Modifiers/LineSpacing.swift
+++ Sources/Ignite/Modifiers/LineSpacing.swift
@@ -30,12 +30,10 @@ public enum LineSpacing: String, CaseIterable, Sendable {
     }
 }
 
-private enum LineSpacingType {
-    case exact(Double), semantic(LineSpacing)
-}
+
 
 @MainActor private func lineSpacingModifier(
-    _ spacing: LineSpacingType,
+    _ spacing: Amount<Double, LineSpacing>,
     content: some InlineElement
 ) -> some InlineElement {
     var modified = ModifiedInlineElement(content)
@@ -86,43 +84,5 @@ public extension ElementProxy {
     /// - Returns: The modified HTML element
     func lineSpacing(_ height: Double) -> Self {
         self.style(.lineHeight, String(height))
-    }
-}
-
-private struct LineSpacedHTML<Content: HTML>: HTML {
-    var attributes = CoreAttributes()
-
-    var body: some HTML { fatalError() }
-
-    var content: Content
-    var spacing: LineSpacingType
-
-    init(_ content: Content, spacing: LineSpacingType) {
-        self.content = content
-        self.spacing = spacing
-    }
-
-    func markup() -> Markup {
-        if content is any TextProvider {
-            switch spacing {
-            case .exact(let spacing):
-                content.style(.init(.lineHeight, value: spacing.formatted(.nonLocalizedDecimal)))
-                    .markup()
-            case .semantic(let spacing):
-                content.class("lh-\(spacing.rawValue)")
-                    .markup()
-            }
-        } else {
-            switch spacing {
-            case .exact(let spacing):
-                Section(content.class("line-height-inherit"))
-                    .style(.lineHeight, spacing.formatted(.nonLocalizedDecimal))
-                    .markup()
-            case .semantic(let spacing):
-                Section(content.class("line-height-inherit"))
-                    .class("lh-\(spacing.rawValue)")
-                    .markup()
-            }
-        }
     }
 }
