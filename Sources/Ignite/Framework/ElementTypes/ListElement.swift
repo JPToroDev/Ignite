c293256bab3f784c16ccc260efc7b6e2a1daeaac
JP Toro <jp@jptoro.dev>
2025-05-23 11:31:34 -0400

    Remove `MarkupElement` inheritance from `ListElement`.

--- Sources/Ignite/Framework/ElementTypes/ListElement.swift
+++ Sources/Ignite/Framework/ElementTypes/ListElement.swift
@@ -7,12 +7,15 @@
 
 /// An element that handles list rendering by placing its <li> tag manually.
 @MainActor
-protocol ListElement: MarkupElement {
+protocol ListElement {
+    /// The standard set of control attributes for HTML elements.
+    var attributes: CoreAttributes { get set }
+
     /// Render this when we know for sure we're part of a `List`.
     func markupAsListItem() -> Markup
 }
 
-extension ListElement {
+extension ListElement where Self: HTML {
     /// Renders this element inside a list.
     /// - Returns: The HTML for this element.
     func markupAsListItem() -> Markup {
