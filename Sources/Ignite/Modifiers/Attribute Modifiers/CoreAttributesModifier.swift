4276f1b882875d2fa675025a44eea6e111a7c012
JP Toro <jp@jptoro.dev>
2025-05-23 10:58:40 -0400

    Correct access level of `attributes()` modifier.

--- Sources/Ignite/Modifiers/Attribute Modifiers/CoreAttributesModifier.swift
+++ Sources/Ignite/Modifiers/Attribute Modifiers/CoreAttributesModifier.swift
@@ -5,59 +5,26 @@
 // See LICENSE for license information.
 //
 
-@MainActor private func coreAttributesModifier(
-    _ attributes: CoreAttributes,
-    content: some HTML
-) -> some HTML {
-    var modified = ModifiedHTML(content)
-    modified.attributes.merge(attributes)
-    return modified
-}
-
-@MainActor private func coreAttributesModifier(
-    _ attributes: CoreAttributes,
-    content: some InlineElement
-) -> some InlineElement {
-    var modified = ModifiedInlineElement(content)
-    modified.attributes.merge(attributes)
-    return modified
-}
-
-//@MainActor private func coreAttributesModifier(
-//    _ attributes: CoreAttributes,
-//    content: any BodyElement
-//) -> any BodyElement {
-//    var copy = content
-//    copy.attributes.merge(attributes)
-//    return copy
-//}
-
-public extension HTML {
+extension HTML {
     /// Merges a complete set of core attributes into this element.
     /// - Parameter attributes: The CoreAttributes to merge with existing attributes
     /// - Returns: The modified `HTML` element
     /// - Note: Uses AttributeStore for persistent storage and merging
     func attributes(_ attributes: CoreAttributes) -> some HTML {
-        coreAttributesModifier(attributes, content: self)
+        var modified = ModifiedHTML(self)
+        modified.attributes.merge(attributes)
+        return modified
     }
 }
 
-public extension InlineElement {
+extension InlineElement {
     /// Merges a complete set of core attributes into this element.
     /// - Parameter attributes: The CoreAttributes to merge with existing attributes
     /// - Returns: The modified `HTML` element
-    /// - Note: Uses AttributeStore for persistent storage and merging
+    /// - Note: Uses AttributeStore for persistent storagex and merging
     func attributes(_ attributes: CoreAttributes) -> some InlineElement {
-        coreAttributesModifier(attributes, content: self)
+        var modified = ModifiedInlineElement(self)
+        modified.attributes.merge(attributes)
+        return modified
     }
 }
-
-//extension BodyElement {
-//    /// Merges a complete set of core attributes into this element.
-//    /// - Parameter attributes: The CoreAttributes to merge with existing attributes
-//    /// - Returns: The modified HTML element
-//    /// - Note: Uses AttributeStore for persistent storage and merging
-//    func attributes(_ attributes: CoreAttributes) -> some BodyElement {
-//        AnyHTML(coreAttributesModifier(attributes, content: self))
-//    }
-//}
