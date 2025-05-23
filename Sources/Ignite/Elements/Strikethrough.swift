1d9fe016119131d61313aaaea5cd8191bf18e6ec
JP Toro <jp@jptoro.dev>
2025-05-23 11:17:40 -0400

    Update `Strikethrough` to use generics.

--- Sources/Ignite/Elements/Strikethrough.swift
+++ Sources/Ignite/Elements/Strikethrough.swift
@@ -6,26 +6,26 @@
 //
 
 /// Renders text with a strikethrough effect.
-public struct Strikethrough: InlineElement {
+public struct Strikethrough<Content: InlineElement>: InlineElement {
     /// The content and behavior of this HTML.
-    public var body: some InlineElement { fatalError() }
+    public var body: Never { fatalError() }
 
     /// The standard set of control attributes for HTML elements.
     public var attributes = CoreAttributes()
 
     /// The content that should be stricken.
-    var content: any InlineElement
+    var content: Content
 
     /// Creates a new `Strikethrough` instance using an inline element builder
     /// that returns an array of content to place inside.
-    public init(@InlineElementBuilder content: () -> some InlineElement) {
+    public init(@InlineElementBuilder content: () -> Content) {
         self.content = content()
     }
 
     /// Creates a new `Strikethrough` instance using one `InlineElement`
     /// that should be rendered with a strikethrough effect.
     /// - Parameter singleElement: The element to strike.
-    public init(_ singleElement: any InlineElement) {
+    public init(_ singleElement: Content) {
         self.content = singleElement
     }
 
