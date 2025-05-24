51b8219e0182879b077460b93cee983295a09d61
JP Toro <jp@jptoro.dev>
2025-05-24 09:16:01 -0400

    Restore missing `TagMetadata` type.

---
+++ Sources/Ignite/Framework/TagMetadata.swift
@@ -0,0 +1,11 @@
+//
+// TagMetadata.swift
+// Ignite
+// https://www.github.com/twostraws/Ignite
+// See LICENSE for license information.
+//
+
+public struct TagMetadata: Sendable {
+    public var name: String
+    public var path: String
+}
